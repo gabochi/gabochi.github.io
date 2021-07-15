78ac4a2da8dd3cb5655d569baa3d5e0df386a24a
Gabriel Vinazza <28355899+gabochi@users.noreply.github.com>
2021-07-15 17:20:01 -0300

    updated with script

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo restarting index.html
 rm index.html
 cp base.html index.html
 
+echo adding posts
 for filename in $(ls posts/* | sort)
 do
-	sed "35a <p><a href=\"#${filename:6}\">${filename}</a> $(cat ${filename}) </p>" index.html -i
+	sed "35a <h2 id=\"${filename:6}\">${filename}</h2> <p> $(cat ${filename}) </p>" index.html -i && echo ${filename} added || echo ${filename} failed
 done
 
+echo pushing repository
 git add .
 git commit -m "updated with script"
 git push
