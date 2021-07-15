4fb2620637c2d81b47a28ffa2751de9f5f6aceaa
Gabriel Vinazza <28355899+gabochi@users.noreply.github.com>
2021-07-15 17:44:10 -0300

    updated with script

--- update.sh
+++ update.sh
@@ -7,8 +7,11 @@ cp base.html index.html
 echo adding posts
 for filename in $(ls posts/* | sort)
 do
-	sed "35a <h2 id=\"${filename:6}\">${filename}</h2> <p> $(cat ${filename}) </p>" index.html -i && echo ${filename} added || echo ${filename} failed
+	sed "35a <h3 id=\"${filename:6}\">${filename}</h3> <p> $(cat ${filename}) </p>" index.html -i && echo ${filename} added || echo ${filename} failed
+	allposts="${allposts}<a href=\"#${filename:6}\">${filename:6}</a>"
 done
+
+sed "35a ${allposts}" index.html -i
 
 echo pushing repository
 git add .
