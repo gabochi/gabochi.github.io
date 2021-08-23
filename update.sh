1d4812af7bb2c8bec2d192cdd560cabaf80f26f3
Gabriel Vinazza <28355899+gabochi@users.noreply.github.com>
2021-08-23 00:43:44 -0300

    Add files via upload

--- update.sh
+++ update.sh
@@ -7,14 +7,14 @@ cp base.html index.html
 echo adding posts
 for filename in $(ls posts/* | sort)
 do
-	sed "35a <h2 id=\"${filename:6}\">${filename}</h2> <p> $(cat ${filename}) </p>" index.html -i && echo ${filename} added || echo ${filename} failed
-	allposts="<a href=\"#${filename:6}\">${filename:6}</a>${allposts}"
+	sed "66a <h2 id=\"${filename:6}\">${filename}</h2> <p> $(cat ${filename}) </p>" index.html -i && echo ${filename} added || echo ${filename} failed
+	allposts="<a href=\"#${filename:6}\">${filename:6}</a><br>${allposts}"
 done
 
 echo adding links
-sed "36a ${allposts}" index.html -i
+sed "67a ${allposts}" index.html -i
 
-#[ $1 != "" ] && exit
+[ "$1" != "push" ] && echo "exiting (use 'push' arg to update the repo)..." && exit
 
 echo pushing repo
 git add .
